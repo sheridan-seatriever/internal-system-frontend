7d3177385da26a66c7f53d6901c7f66cba493e9d
sheridan-seatriever <sheridan.pickett@seatriever.com>
2020-10-15 11:02:39 +0100

    EventSidebar inner div now position fixed

--- src/Components/Calendar/EventSidebar/EventSidebar.js
+++ src/Components/Calendar/EventSidebar/EventSidebar.js
@@ -37,40 +37,43 @@ const EventSidebar = ({currentEventID, setCurrentEventID, users, fetchUsersError
   }
 
   return (
-    <div className={`${styles.container} ${currentEventID&&styles.open}`}>
-      <div className={styles.button_group}>
-        <button type="button" className={`${styles.cancel}`} onClick={()=>setCurrentEventID('')}>CANCEL</button>
-      </div>
-      <div className={'form_element label_group'}>
-        <label>Project Title:</label>
-        <input className={styles.input} value={title} onChange={e=>setTitle(e.target.value)}/>
-      </div>
-      <div className={'form_element label_group'}>
-        <label>Project Description:</label>
-        <textarea className={`${styles.input} ${styles.input_large}`} value={description} onChange={e=>setDescription(e.target.value)}></textarea>
-      </div>
-      <div className={'form_element label_group'}>
-        <label>Project Manager:</label>
-        {loadingUsers?
+    <>
+      <div className={`${styles.buffer} ${currentEventID&&styles.open}`}></div>
+      <div className={`${styles.container} ${currentEventID&&styles.open}`}>
+        <div className={styles.button_group}>
+          <button type="button" className={`${styles.cancel}`} onClick={()=>setCurrentEventID('')}>CANCEL</button>
+        </div>
+        <div className={'form_element label_group'}>
+          <label>Project Title:</label>
+          <input className={styles.input} value={title} onChange={e=>setTitle(e.target.value)}/>
+        </div>
+        <div className={'form_element label_group'}>
+          <label>Project Description:</label>
+          <textarea className={`${styles.input} ${styles.input_large}`} value={description} onChange={e=>setDescription(e.target.value)}></textarea>
+        </div>
+        <div className={'form_element label_group'}>
+          <label>Project Manager:</label>
+          {loadingUsers?
+            <div>loading</div>:
+            <SearchInput data={users} placeholder="Add users"/>
+          }
+        </div>
+        <div className="error">{fetchUsersError}</div>
+        <div className={'form_element label_group'}>
+        <label>Assigned To:</label>
+        {
+          loadingUsers? 
           <div>loading</div>:
-          <SearchInput data={users} placeholder="Add users"/>
+          <AddList data={users.map(user=>user.user_name)} selectedData={assignedTo} setSelectedData={setAssignedTo}/>
         }
+        </div>
+        <div className="error">{fetchUsersError}</div>
+        <div className={styles.button_group}>
+          <button type="button" className={`${styles.delete}`} onClick={deleteProject}>DELETE PROJECT</button>
+          <button type="button" className={`${'button-primary'} ${styles.button_primary}`} onClick={deleteProject}>UPDATE PROJECT</button>
+        </div>
       </div>
-      <div className="error">{fetchUsersError}</div>
-      <div className={'form_element label_group'}>
-      <label>Assigned To:</label>
-      {
-        loadingUsers? 
-        <div>loading</div>:
-        <AddList data={users.map(user=>user.user_name)} selectedData={assignedTo} setSelectedData={setAssignedTo}/>
-      }
-      </div>
-      <div className="error">{fetchUsersError}</div>
-      <div className={styles.button_group}>
-        <button type="button" className={`${styles.delete}`} onClick={deleteProject}>DELETE PROJECT</button>
-        <button type="button" className={`${'button-primary'} ${styles.button_primary}`} onClick={deleteProject}>UPDATE PROJECT</button>
-      </div>
-    </div>
+    </>
   )
 }
 
